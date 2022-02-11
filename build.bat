9ba06d60e5304c4472b52ced3beadabccb09c2de
Bernát Gábor <bgabor8@bloomberg.net>
2022-02-11 18:21:24 +0000

    Add Github build
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- build.bat
+++
@@ -1,3 +0,0 @@
-cd src
-pdflatex.exe -synctex=1 -interaction=nonstopmode -aux-directory="..\out" -output-directory="..\out" -job-name=rendszeroptimizalas_vizsgajegyzet --shell-escape --src-specials "main.tex"
-cd..
